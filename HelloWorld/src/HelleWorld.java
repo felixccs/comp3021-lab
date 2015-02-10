6cb983dd940693b5b7aec992937809378ddecf62
felixccs <felixsupport@gmail.xom>
2015-02-10 12:47:11 +0800

    Update HelleWorld.java

--- HelloWorld/src/HelleWorld.java
+++ HelloWorld/src/HelleWorld.java
@@ -6,7 +6,7 @@ public class HelleWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello World");
+			System.out.println("Hello World, Comp3021, lab 1");
 	}
 
 }
